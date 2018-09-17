d568f42195d23b9fd3c793f970c27b1a950a55af
Leandro Masello <leandro.masello@wolox.com.ar>
2018-09-17 16:36:22 -0300

    Add more tests for course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -6,4 +6,8 @@ describe Course do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:vacancies) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  it { is_expected.to validate_numericality_of(:vacancies).only_integer }
+  it { is_expected.to validate_numericality_of(:vacancies).is_less_than_or_equal_to(50) }
+  it { is_expected.to validate_numericality_of(:vacancies).is_greater_than_or_equal_to(0) }
 end
