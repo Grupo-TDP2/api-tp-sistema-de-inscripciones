fc8cece473f47e41f380d102925665ba8d345263
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-17 17:19:12 -0300

    Add validations de class model

--- app/models/lesson_study.rb
+++ app/models/lesson_study.rb
@@ -1,24 +1,19 @@
 class LessonStudy < ApplicationRecord
-  validates :term, :year, :date_start, :date_end, presence: true
-  validates :year, numericality: { only_integer: true, greater_than: 2017, less_than: 2050 }
-  validates :term, uniqueness: { scope: :year, case_sensitive: false }
-  validate :validate_year_start_end, :validate_start_end, if: %i[date_start date_end]
+  belongs_to :course
+  belongs_to :classroom
+  validates :type, :day, :hour_start, :hour_end, :date_end, presence: true
+  validate :validate_start_end
 
-  enum term: { first_semester: 0, second_semester: 1 }
+  references :course, index: true, foreign_key: true
+  references :classroom, index: true, foreign_key: true
+
+  enum type: { theory: 0, practice: 1 }
 
   private
 
-  def validate_year_start_end
-    return if date_start.year == year && date_end.year == year
-    date_start.year != year ? year_error(:date_start) : year_error(:date_end)
-  end
-
   def validate_start_end
-    return if date_start < date_end
-    errors.add(:date_start, 'cannot be greater than date end')
+    return if hour_start < hour_end
+    errors.add(:hour_start, 'cannot be greater than hour end')
   end
 
-  def year_error(attribute)
-    errors.add(attribute, 'must have the same input year')
-  end
 end
