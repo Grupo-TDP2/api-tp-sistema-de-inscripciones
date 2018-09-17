22b8f6a9f769b9f8f2de254db6aef7f359531e1b
Leandro Masello <leandro.masello@wolox.com.ar>
2018-09-17 00:12:10 -0300

    Add specs

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -13,4 +13,18 @@ describe Subject do
   it { is_expected.to validate_numericality_of(:credits).only_integer }
   it { is_expected.to validate_numericality_of(:credits).is_less_than_or_equal_to(10) }
   it { is_expected.to validate_numericality_of(:credits).is_greater_than(0) }
+
+  context 'when adding correlatives' do
+    let!(:another_subject) { create(:subject) }
+
+    before { another_subject.correlative_subjects << described_class.first }
+
+    it 'has a correlative_subject' do
+      expect(another_subject.correlative_subjects.size).to eq 1
+    end
+
+    it 'adds the inverse correlativity' do
+      expect(described_class.first.needed_subjects.first).to eq another_subject
+    end
+  end
 end
