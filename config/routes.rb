2b9321698287f7cd3054dafe9e9b986444a4d97b
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-09-18 17:21:32 -0300

    Reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'application#index'
-  devise_for :admins
   devise_for :students
   devise_for :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
