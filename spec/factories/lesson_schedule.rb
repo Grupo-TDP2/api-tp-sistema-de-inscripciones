9138b1fe93fc690642ad9a3ae8200ebec2f139d8
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-20 00:34:43 -0300

    Change faker to time

--- spec/factories/lesson_schedule.rb
+++ spec/factories/lesson_schedule.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :lesson_schedule do
     type { Faker::Number.between(0, 1) }
     day { Faker::Date.between(Time.zone.today, 10.years.from_now).day }
-    hour_start { Faker::Date.between("09:00", "12:00") }
-    hour_end { Faker::Date.between("13:00", "15:00") }
+    hour_start { Faker::Time.between(Date.today, Date.today, :day) }
+    hour_end { Faker::Time.between(Date.today, Date.today, :night) }
   end
 end
