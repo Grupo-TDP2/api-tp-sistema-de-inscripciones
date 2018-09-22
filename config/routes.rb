3bf7e2fbbe78c86bce78e7af5a96d2d3289d069e
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-09-22 10:54:06 -0300

    Add teacher model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'application#index'
   devise_for :admins
   devise_for :students
+  devise_for :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   api_version(module: 'v1', path: { value: 'api/v1' }, defaults: { format: :json }) do
     resources :course_of_studies, only: [] do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180921214704) do
+ActiveRecord::Schema.define(version: 20180922134416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,24 @@ ActiveRecord::Schema.define(version: 20180921214704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["department_id"], name: "index_subjects_on_department_id"
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "personal_document_number", null: false
+    t.date "birthdate", null: false
+    t.string "phone_number", null: false
+    t.string "address", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_teachers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
   add_foreign_key "classrooms", "buildings"
