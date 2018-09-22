52def938e8ae3dd46056a82bc32ba4f7d8edef1e
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-09-22 17:55:16 -0300

    Fix random specs

--- spec/models/teacher_course_spec.rb
+++ spec/models/teacher_course_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe TeacherCourse do
-  before { create(:teacher_course) }
-
   it { is_expected.to validate_presence_of(:teaching_position) }
   it { is_expected.to validate_presence_of(:teacher_id) }
   it { is_expected.to validate_presence_of(:course_id) }
-  it { is_expected.to validate_uniqueness_of(:teacher_id).scoped_to(:course_id) }
+
+  context 'when checking for uniqueness of teacher per course' do
+    before { create(:teacher_course) }
+    it { is_expected.to validate_uniqueness_of(:teacher_id).scoped_to(:course_id) }
+  end
 
   context 'when selecting unique positions' do
     before { create(:teacher_course, teaching_position: 0) }
@@ -19,7 +21,8 @@ describe TeacherCourse do
   context 'when selecting not unique positions' do
     before { create(:teacher_course, teaching_position: 2) }
     it do
-      is_expected.not_to validate_uniqueness_of(:teaching_position).scoped_to(:course_id)
+      is_expected.not_to validate_uniqueness_of(:teaching_position).ignoring_case_sensitivity
+                                                                   .scoped_to(:course_id)
     end
   end
 end
