1148df6dbb33e534864f053d6bcd7107be78c42d
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-22 18:46:23 -0300

    tipe error

--- spec/factories/lesson_schedule.rb
+++ spec/factories/lesson_schedule.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     type { Faker::Number.between(0, 1) }
     day { Faker::Number.between(0, 5) }
     hour_start { Faker::Time.between(Time.zone.today, Time.zone.today, :day).strftime('%H:%M') }
-    hour_end { Faker::Time.between(Time.zone.today, Time.zone.today, :nigth).strftime('%H:%M') }
+    hour_end { Faker::Time.between(Time.zone.today, Time.zone.today, :night).strftime('%H:%M') }
     course { create(:course) }
     classroom { create(:classroom) }
   end
