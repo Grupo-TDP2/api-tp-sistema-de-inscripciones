b063d9f13423d76af9ab458c4293d4c6012e61e3
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-22 19:06:02 -0300

    Add input to validation

--- app/models/lesson_schedule.rb
+++ app/models/lesson_schedule.rb
@@ -3,7 +3,7 @@ class LessonSchedule < ApplicationRecord
   belongs_to :classroom
   self.inheritance_column = :_type_disabled
   validates :type, :day, :hour_start, :hour_end, presence: true
-  validate :validate_start_end
+  validate :validate_start_end, if: %i[hour_start hour_end]
 
   enum type: { theory: 0, practice: 1 }
   enum day: { Monday: 0, Tuesday: 1, Wednesday: 2, Thursday: 3, Friday: 4, Saturday: 5 }
