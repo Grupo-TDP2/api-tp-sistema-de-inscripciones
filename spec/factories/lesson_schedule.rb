6c41e3edfa878533bbee2eccdd69365c5e24d4b2
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-22 18:26:22 -0300

    Fix test

--- spec/factories/lesson_schedule.rb
+++ spec/factories/lesson_schedule.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     day { Faker::Number.between(0, 5) }
     hour_start { Faker::Time.between(Time.zone.today, Time.zone.today, :day).strftime('%H:%M') }
 
-    hour_end { hour_start + 3.hours }
+    hour_end { (hour_start + 3.hours).to_s }
     course { create(:course) }
     classroom { create(:classroom) }
   end
