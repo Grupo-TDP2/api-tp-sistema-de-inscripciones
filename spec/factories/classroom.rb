c7a45a94db4a1526e059aed47c35ef347aac4d6b
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-23 00:31:33 -0300

    Add relation to classroom fix

--- spec/factories/classroom.rb
+++ spec/factories/classroom.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     floor { Faker::Number.number(1) }
     number { Faker::Number.number(3) }
     building { create(:building) }
-    has_many :lesson_schedules, dependent: :destroy
   end
 end
