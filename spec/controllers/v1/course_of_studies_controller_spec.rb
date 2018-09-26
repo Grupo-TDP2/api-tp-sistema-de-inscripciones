96d31fec076fb69af5c8de0524b55ba6098e0f43
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-26 20:27:17 -0300

    Change spec controller name

--- spec/controllers/v1/course_of_studies_controller_spec.rb
+++ spec/controllers/v1/course_of_studies_controller_spec.rb
@@ -1,29 +1,32 @@
 require 'rails_helper'
- describe V1::CoursesOfStudyController do
+
+describe V1::CourseOfStudiesController do
   describe '#index' do
     let(:course_of_study_subject) { create(:course_of_study_subject) }
-    let(:test_course_of_study) { course_of_study_subject.course_of_study }
     let(:index_request) do
       get :index
     end
-     context 'when there are two courses of study' do
-       let(:another_course_of_study) { create(:course_of_study, name: 'Ingeniería Informática', required_credits: 260)}
 
-       before { course_of_study_subject.course_of_study << another_course_of_study }
+    context 'when there are two courses of study' do
+      let(:another_course_of_study) do
+        create(:course_of_study, name: 'Ingeniería Informática', required_credits: 260)
+      end
+
+      before { course_of_study_subject.course_of_study = another_course_of_study }
 
       it 'returns two courses of study' do
         index_request
         expect(response_body.size).to eq 2
       end
-       it 'returns http status ok' do
+      it 'returns http status ok' do
         index_request
         expect(response).to have_http_status :ok
       end
-       it 'returns the right keys' do
+      it 'returns the right keys' do
         index_request
         expect(response_body.first.keys)
           .to match_array(%w[id name required_credits])
       end
-
-      end
     end
+  end
+end
