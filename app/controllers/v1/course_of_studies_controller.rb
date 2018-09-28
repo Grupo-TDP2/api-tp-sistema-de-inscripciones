4cefa0949b3e8c026f7c4f37aeaf92ba8f99928c
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-09-27 23:21:40 -0300

    Add skip authentication before index controller

--- app/controllers/v1/course_of_studies_controller.rb
+++ app/controllers/v1/course_of_studies_controller.rb
@@ -1,5 +1,7 @@
 module V1
   class CourseOfStudiesController < ApplicationController
+    skip_before_action :authenticate_user!, only: %i[index]
+
     def index
       render json: course_of_studies, status: :ok
     end
