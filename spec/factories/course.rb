4a4ef3152b136486c05c834d68eeb763bc7a4429
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-09-29 16:36:44 -0300

    Modify factory

--- spec/factories/course.rb
+++ spec/factories/course.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :course do
-    name { Faker::Commerce.department }
+    name { Faker::Number.between(1, 999) }
     vacancies { Faker::Number.between(1, 50) }
     school_term { create(:school_term) }
     subject { create(:subject) }
