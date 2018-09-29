bf670f24d6b597e1e8fb91c43fc12521a8ecd365
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-09-29 12:19:30 -0300

    Use inheritance for session controllers

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,12 +1,17 @@
 module V1
   class SessionsController < ApplicationController
-    include SessionConcern
+    def create
+      if authenticated_user?
+        render json: {
+          access_token: user.authentication_token,
+          expires_in: Rails.application.secrets.expiration_date_days.days.seconds
+        }, status: :ok
+      else
+        render json: { error: 'Credenciales inválidas' }, status: :unauthorized
+      end
+    end
 
     private
-
-    def user
-      @user ||= Student.find_by(email: authenticate_params[:email])
-    end
 
     def authenticated_user?
       user.present? && user.valid_password?(authenticate_params[:password])
--- app/controllers/v1/teacher_sessions_controller.rb
+++ app/controllers/v1/teacher_sessions_controller.rb
@@ -1,19 +1,7 @@
 module V1
-  class TeacherSessionsController < ApplicationController
-    include SessionConcern
-
-    private
-
+  class TeacherSessionsController < SessionsController
     def user
       @user ||= Teacher.find_by(email: authenticate_params[:email])
-    end
-
-    def authenticated_user?
-      user.present? && user.valid_password?(authenticate_params[:password])
-    end
-
-    def authenticate_params
-      params.permit(:email, :password)
     end
   end
 end
