fe66f9c839ce855743fc1525fb7ba4da199f9287
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-09-30 22:50:51 -0300

    Enable CORS

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module ApiTpSistemaDeInscripciones
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :es
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      }
   end
 end
