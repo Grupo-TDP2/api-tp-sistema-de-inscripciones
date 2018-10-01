7b4ca947eff6404241de04ae40cdd56ad340f29a
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-10-01 16:50:39 -0300

    Fix NoMethodName error

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,5 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :vacancies, :inscribed
+  attributes :id, :name, :vacancies, :inscribed?
   belongs_to :subject
   belongs_to :school_term
   has_many :teachers
