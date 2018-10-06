890341a50389bf56b454618e70cc6720ce0a14d8
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-10-06 20:28:23 -0300

    Do not let teachers to have more than one course per subject

--- app/models/teacher_course.rb
+++ app/models/teacher_course.rb
@@ -3,6 +3,7 @@ class TeacherCourse < ApplicationRecord
   validates :teaching_position, uniqueness: { scope: :course_id, case_sensitive: false },
                                 if: :unique_position?
   validates :teacher_id, uniqueness: { scope: :course_id }
+  validate :unique_course_per_subject
 
   belongs_to :teacher
   belongs_to :course
@@ -14,5 +15,12 @@ class TeacherCourse < ApplicationRecord
 
   def unique_position?
     course_chief? || practice_chief?
+  end
+
+  def unique_course_per_subject
+    subject_courses_ids = course&.subject&.courses&.map(&:id) || []
+    teacher_courses = TeacherCourse.where(teacher: teacher)
+    errors.add(:teacher_id, 'cannot have more than one course per subject') if
+       teacher_courses.any? { |course| subject_courses_ids.include?(course.course_id) }
   end
 end
