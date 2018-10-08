532e8d5e6e5b8d2d2a87d2797b519d676aa940e4
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-10-08 01:53:12 -0300

    Add index test school term

--- spec/controllers/v1/school_terms_controller_spec.rb
+++ spec/controllers/v1/school_terms_controller_spec.rb
@@ -31,4 +31,67 @@ describe V1::SchoolTermsController do
       end
     end
   end
+
+  describe '#index' do
+    let(:school_term) { create(:school_term) }
+    let(:current_admin) { create(:admin) }
+    let(:index_request) do
+      get :index
+    end
+
+    context 'with no admin logged in' do
+      it 'returns unauthorized' do
+        index_request
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'when the admin is logged in' do
+      before { sign_in current_admin }
+
+      context 'when there is one school term' do
+        before do
+          create(:school_term, year: '2018', term: :second_semester, date_start: '2018-08-01',
+                               date_end: '2018-12-01')
+        end
+        it 'returns http status ok' do
+          index_request
+          expect(response).to have_http_status :ok
+        end
+
+        it 'returns the right keys' do
+          index_request
+          expect(response_body.first.keys)
+            .to match_array(%w[id term year date_start date_end])
+        end
+        it 'only returns one school term' do
+          index_request
+          expect(response_body.size).to eq 1
+        end
+      end
+
+      context 'when there are two school terms' do
+        before do
+          create(:school_term, year: '2018', term: :second_semester, date_start: '2018-08-01',
+                               date_end: '2018-12-01')
+          create(:school_term, year: '2018', term: :first_semester, date_start: '2018-03-01',
+                               date_end: '2018-07-01')
+        end
+        it 'returns http status ok' do
+          index_request
+          expect(response).to have_http_status :ok
+        end
+
+        it 'returns the right keys' do
+          index_request
+          expect(response_body.first.keys)
+            .to match_array(%w[id term year date_start date_end])
+        end
+        it 'only returns two school term' do
+          index_request
+          expect(response_body.size).to eq 2
+        end
+      end
+    end
+  end
 end
