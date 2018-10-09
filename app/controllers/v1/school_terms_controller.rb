bbb7b20af0d337d8d18b2cc535e06a5336ca42ca
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-10-09 20:29:31 -0300

    Add tests and fix controller issues

--- app/controllers/v1/school_terms_controller.rb
+++ app/controllers/v1/school_terms_controller.rb
@@ -6,25 +6,32 @@ module V1
     end
 
     def create
-      @school_term = SchoolTerm.new(school_term_params)
-      @school_term.save
-      render json: @school_term, status: :created
+      school_term = SchoolTerm.new(school_term_params)
+      if school_term.save
+        render json: school_term, status: :created
+      else
+        render json: school_term.errors.full_messages, status: :unprocessable_entity
+      end
     end
 
     def show
-      @school_term = SchoolTerm.find(params[:id])
-      render json: @school_term, status: :ok
+      school_term = SchoolTerm.find(params[:id])
+      render json: school_term, status: :ok
     end
 
     def destroy
-      @school_term = SchoolTerm.find(params[:id])
-      @school_term.destroy
+      school_term = SchoolTerm.find(params[:id])
+      if school_term.destroy
+        head :ok
+      else
+        render json: school_term.errors.full_messages, status: :unprocessable_entity
+      end
     end
 
     private
 
     def school_term_params
-      params.permit(:school_term).permit(:term, :year, :date_start, :date_end)
+      params.require(:school_term).permit(:term, :year, :date_start, :date_end)
     end
 
     def school_terms
--- spec/controllers/v1/school_terms_controller_spec.rb
+++ spec/controllers/v1/school_terms_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe V1::SchoolTermsController do
   describe '#create' do
-    let(:school_term) { create(:school_term) }
     let(:current_admin) { create(:admin) }
     let(:school_term_request) do
-      post :create, params: { term: school_term.term, year: school_term.year,
-                              date_start: school_term.date_start, date_end: school_term.date_end }
+      post :create, params:  { school_term: { term: :first_semester,
+                                              year: 2020,
+                                              date_start: '2020-03-01',
+                                              date_end: '2020-07-01' } }
     end
 
     context 'with no admin logged in' do
@@ -90,6 +91,109 @@ describe V1::SchoolTermsController do
         it 'only returns two school term' do
           index_request
           expect(response_body.size).to eq 2
+        end
+      end
+    end
+  end
+
+  describe '#show' do
+    let(:school_term) do
+      create(:school_term, year: '2018', term: :first_semester,
+                           date_start: '2018-08-01', date_end: '2018-12-01')
+    end
+    let(:current_admin) { create(:admin) }
+    let(:show_request) do
+      get :show, params: { id: school_term.id }
+    end
+
+    context 'with no admin logged in' do
+      it 'returns unauthorized' do
+        show_request
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'when the admin is logged in' do
+      before { sign_in current_admin }
+
+      context 'when there is one school term' do
+        before do
+          create(:school_term, year: '2018', term: :second_semester, date_start: '2018-08-01',
+                               date_end: '2018-12-01')
+        end
+
+        it 'returns http status ok' do
+          show_request
+          expect(response).to have_http_status :ok
+        end
+
+        it 'returns the right keys' do
+          show_request
+          expect(response_body.keys)
+            .to match_array(%w[id term year date_start date_end])
+        end
+      end
+      context 'when there are two school terms' do
+        before do
+          create(:school_term, year: '2019', term: :second_semester, date_start: '2019-08-01',
+                               date_end: '2019-12-01')
+        end
+        it 'returns the rigth school term' do
+          show_request
+          expect(response_body['id']).to eq school_term.id
+        end
+
+        it 'returns the right keys' do
+          show_request
+          expect(response_body.keys)
+            .to match_array(%w[id term year date_start date_end])
+        end
+      end
+    end
+  end
+
+  describe '#destroy' do
+    let(:school_term) { create(:school_term) }
+    let(:current_admin) { create(:admin) }
+    let(:delete_request) do
+      delete :destroy, params: { id: school_term.id }
+    end
+
+    context 'with no admin logged in' do
+      it 'returns unauthorized' do
+        delete_request
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'when the admin is logged in' do
+      before { sign_in current_admin }
+
+      context 'when there is one school term' do
+        before do
+          school_term
+        end
+        it 'returns http status ok' do
+          delete_request
+          expect(response).to have_http_status :ok
+        end
+        it 'deletes an school term' do
+          expect { delete_request }.to change(SchoolTerm, :count).by(-1)
+        end
+      end
+      context 'when there are two school terms' do
+        before do
+          school_term
+          create(:school_term, year: '2018', term: :first_semester, date_start: '2018-03-01',
+                               date_end: '2018-07-01')
+        end
+        it 'returns http status ok' do
+          delete_request
+          expect(response).to have_http_status :ok
+        end
+
+        it 'deletes an school term' do
+          expect { delete_request }.to change(SchoolTerm, :count).by(-1)
         end
       end
     end
