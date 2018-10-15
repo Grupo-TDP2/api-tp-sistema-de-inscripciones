c06d65e31c3b0e35e5e834148fa80dc12bb82c5d
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-10-15 15:10:46 -0300

    Fix spec randomness

--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -14,10 +14,14 @@ describe Exam do
   end
 
   context 'when creating an exam with hour lower than 8 in the morning' do
-    let(:final_exam_week) { create(:final_exam_week) }
+    let(:final_exam_week) { FinalExamWeek.last.presence || create(:final_exam_week) }
+    let!(:school_term) do
+      create(:school_term, year: Date.current.year, term: SchoolTerm.current_term)
+    end
+    let(:course) { create(:course, school_term: school_term) }
     let(:exam) do
       build(:exam, date_time: Time.zone.parse(final_exam_week.date_start_week.to_s) + 7.hours,
-                   final_exam_week: final_exam_week)
+                   final_exam_week: final_exam_week, course: course)
     end
 
     it 'returns the right error' do
@@ -27,10 +31,14 @@ describe Exam do
   end
 
   context 'when creating an exam with hour higher than 21 in the evening' do
-    let(:final_exam_week) { create(:final_exam_week) }
+    let(:final_exam_week) { FinalExamWeek.last.presence || create(:final_exam_week) }
+    let!(:school_term) do
+      create(:school_term, year: Date.current.year, term: SchoolTerm.current_term)
+    end
+    let(:course) { create(:course, school_term: school_term) }
     let(:exam) do
       build(:exam, date_time: Time.zone.parse("#{final_exam_week.date_start_week} 21:30:00"),
-                   final_exam_week: final_exam_week)
+                   final_exam_week: final_exam_week, course: course)
     end
 
     it 'returns the right error' do
@@ -40,10 +48,14 @@ describe Exam do
   end
 
   context 'when creating an exam in a week different than the final exam week it belongs' do
-    let(:final_exam_week) { create(:final_exam_week) }
+    let(:final_exam_week) { FinalExamWeek.last.presence || create(:final_exam_week) }
+    let!(:school_term) do
+      create(:school_term, year: Date.current.year, term: SchoolTerm.current_term)
+    end
+    let(:course) { create(:course, school_term: school_term) }
     let(:exam) do
       build(:exam, date_time: Time.zone.parse("#{final_exam_week.date_start_week + 2.weeks} \
-                                               08:30"))
+                                               08:30"), course: course)
     end
 
     it 'returns the right error' do
