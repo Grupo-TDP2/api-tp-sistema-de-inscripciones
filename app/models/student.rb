9ca454c22c0a5a983ddcc120578986a2e88c966e
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-10-19 12:56:07 -0300

    Change student attributes

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,10 +5,10 @@ class Student < User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, :first_name, :last_name, :personal_document_number, :school_document_number,
-            :birthdate, :phone_number, :address, presence: true
-  validates :phone_number, numericality: true, length: { minimum: 8, maximum: 10 }
-  validates :personal_document_number, numericality: true, length: { is: 8 }
+  validates :email, :first_name, :last_name, :school_document_number, :username, :priority,
+            presence: true
+  validates :priority, numericality: { only_integer: true, greater_than: 0,
+                                       less_than_or_equal_to: 100 }
   validates :school_document_number, numericality: true, length: { minimum: 5, maximum: 6 }
   validates :email, uniqueness: { case_sensitive: false }
   validate :unique_email
