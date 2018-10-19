c36de33c7fc05a60566ffdc4db1ad3ab765c1858
Leandro Masello <lmasello@MacBook-Pro-de-wolox.local>
2018-10-19 01:27:47 -0300

    Add information to exam of courses

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,6 +1,7 @@
 class CourseSerializer < ActiveModel::Serializer
   attributes :id, :name, :vacancies, :accept_free_condition_exam
   attribute :inscribed?, if: :inscribed_student?
+  attribute :enrolment, if: :inscribed_student?
   belongs_to :subject
   belongs_to :school_term
   has_many :teacher_courses
@@ -13,5 +14,9 @@ class CourseSerializer < ActiveModel::Serializer
 
   def inscribed?
     Enrolment.exists?(course: object.id, student: current_user.id)
+  end
+
+  def enrolment
+    Enrolment.find_by(course: object.id, student: current_user.id)
   end
 end
--- app/serializers/exam_serializer.rb
+++ app/serializers/exam_serializer.rb
@@ -1,6 +1,15 @@
 class ExamSerializer < ActiveModel::Serializer
   attributes :id, :exam_type, :date_time
+  attribute :inscribed?, if: :student?
   belongs_to :final_exam_week
   belongs_to :course
   belongs_to :classroom
+
+  def student?
+    current_user.is_a? Student
+  end
+
+  def inscribed?
+    StudentExam.exists?(exam_id: object.id, student: current_user)
+  end
 end
