c3587d2527253e1b974cb05da3e63efe65b264d5
Leandro Masello <leandro.masello@wolox.com.ar>
2018-10-20 13:52:22 -0300

    Add info to exam endpoints

--- app/serializers/exam_serializer.rb
+++ app/serializers/exam_serializer.rb
@@ -1,6 +1,7 @@
 class ExamSerializer < ActiveModel::Serializer
   attributes :id, :exam_type, :date_time
   attribute :inscribed?, if: :student?
+  attribute :registration, if: :inscribed?
   belongs_to :final_exam_week
   belongs_to :course
   belongs_to :classroom
@@ -11,5 +12,9 @@ class ExamSerializer < ActiveModel::Serializer
 
   def inscribed?
     StudentExam.exists?(exam_id: object.id, student: current_user)
+  end
+
+  def registration
+    StudentExam.find_by(exam_id: object.id, student: current_user)
   end
 end
