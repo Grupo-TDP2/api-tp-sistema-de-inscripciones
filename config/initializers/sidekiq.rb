39d153a631f2b9173ff104208c947f02ffae5527
Leandro Masello <leandro.masello@wolox.com.ar>
2018-10-28 23:29:26 -0300

    Add sidekiq config file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,26 @@ schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
+# Sidekiq configuration file
+
+url = ''
+url = if ENV['REDISCLOUD_URL']
+        ENV['REDISCLOUD_URL']
+      elsif ENV['REDISTOGO_URL']
+        ENV['REDISTOGO_URL']
+      elsif ENV['REDIS_URL']
+        ENV['REDIS_URL']
+      else
+        "redis://#{ENV.fetch('REDIS_1_PORT_6379_TCP_ADDR', '127.0.0.1')}:6379"
+      end
+
+Sidekiq.configure_server do |config|
+  config.redis = { size: 7, url: url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: url }
+end
+
+Sidekiq.default_worker_options = { 'backtrace' => true }
