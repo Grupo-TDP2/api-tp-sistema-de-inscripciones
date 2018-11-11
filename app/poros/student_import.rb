81e59d2e29ef619f0c4a551e8340fc2aaa46bbe4
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-11-11 13:23:40 -0300

    Only for test

--- app/poros/student_import.rb
+++ app/poros/student_import.rb
@@ -5,7 +5,11 @@ class StudentImport
   end
 
   def process
-    # process csv file
+    CSV.parse(params[:file], :col_sep => ';') do |row|
+      #byebug
+      create(:student, row)
+      p row
+    end
     @import_file.update(rows_successfuly_processed: 0, rows_unsuccessfuly_processed: 0)
     @import_file
   end
