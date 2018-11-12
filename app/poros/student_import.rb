3dcb0e893dd2d0c407dd06b5c14fd6af6921bb58
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-11-12 03:17:34 -0300

    Horrible changes with csv issues

--- app/poros/student_import.rb
+++ app/poros/student_import.rb
@@ -5,29 +5,35 @@ class StudentImport
   end
 
   def process
-    convert = {"nombre" => "first_name",
-               "apellido" => "last_name",
-               "mail" => "email",
-               "clave" => "password",
-               "padrón" => "school_document_number",
-               "usuario" => "username",
-               "prioridad" => "priority"}
+    convert = {"nombre" => "first_name", "apellido" => "last_name", "mail" => "email",
+               "clave" => "password", "padrón" => "school_document_number",
+               "usuario" => "username", "prioridad" => "priority"}
+    expected_headers = ["first_name", "last_name", "email",
+               "password", "school_document_number", "username", "priority"]
     success = 0
     failed = 0
     line = 0
     proccesed_errors = ""
     csv = CSV.new(@file, :headers => true, :header_converters => lambda { |header| convert[header.downcase.strip] },
                   :converters => lambda {|field| field ? field.strip : nil})
+    csv_headers = CSV.new(@file, :headers => true, :header_converters => lambda { |header| convert[header.downcase.strip] }).read
+
+    file_headers = csv_headers.headers
+    raise ArgumentError unless expected_headers.all? { |header| file_headers.include?(header) } && expected_headers.size == file_headers.size
+
     csv.to_a.map do |row|
       student = Student.new(row.to_hash)
-      if student.save
+      byebug
+      if row.to_hash.values.last == nil
+        proccesed_errors += '- Linea ' + line.to_s + ': Número de columnas erroneo\n'
+        failed += 1
+      elsif student.save
         success += 1
       else
         proccesed_errors += '- Linea ' + line.to_s + ': ' + student.errors.full_messages.to_s + '\n'
         failed += 1
       end
       line += 1
-      p proccesed_errors
     end
     @import_file.update(rows_successfuly_processed: success, rows_unsuccessfuly_processed: failed,
                         proccesed_errors: proccesed_errors)
