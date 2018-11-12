6d99094724bb5487f0ec5d17d9e67039a399c0b9
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-11-12 00:27:23 -0300

    add attribute to import file

--- app/models/import_file.rb
+++ app/models/import_file.rb
@@ -1,6 +1,6 @@
 class ImportFile < ApplicationRecord
   validates :filename, :model, :rows_successfuly_processed, :rows_unsuccessfuly_processed,
-            presence: true
+            :proccesed_errors, presence: true
 
   enum model: { student: 0, teacher: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181110162241) do
+ActiveRecord::Schema.define(version: 20181112030023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 20181110162241) do
     t.integer "rows_unsuccessfuly_processed", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "proccesed_errors"
   end
 
   create_table "lesson_schedules", force: :cascade do |t|
