7276c0cb1ce4a5e4b37c43508be3df7e0e901295
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-11-12 15:00:36 -0300

    Add attribute school_doc to teacher

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181112030023) do
+ActiveRecord::Schema.define(version: 20181112173825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -232,6 +232,7 @@ ActiveRecord::Schema.define(version: 20181112030023) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", null: false
+    t.integer "school_document_number"
     t.index ["email"], name: "index_teachers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
