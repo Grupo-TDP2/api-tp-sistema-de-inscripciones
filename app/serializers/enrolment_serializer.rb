e5e1875a77f9a63bffe6553e715f9ae3c093da21
Leandro Masello <leandro.masello@wolox.com.ar>
2018-11-21 16:10:46 -0300

    Add exam qualification to enrolment list

--- app/serializers/enrolment_serializer.rb
+++ app/serializers/enrolment_serializer.rb
@@ -1,5 +1,15 @@
 class EnrolmentSerializer < ActiveModel::Serializer
   attributes :id, :type, :partial_qualification, :status, :final_qualification, :created_at
+  attribute :exam_qualification
   belongs_to :student
   belongs_to :course
+
+  def qualified?
+    object.final_qualification.present?
+  end
+
+  def exam_qualification
+    StudentExam.where.not(qualification: nil)
+               .where(student_id: object.student.id, exam_id: object.course.exams.map(&:id)).last
+  end
 end
